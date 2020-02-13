64d9e3c54cdfb76fd6d6a2bfd0ea305d0e07504a
Ryan Seit <ryan@ryanseit.com>
2020-02-13 13:52:31 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,12 @@
 # Your Code Here
+def map(enumerable, &block)
+  result = []
+  enumerable.map {|element| result << block.call(element)}
+  result
+end
+
+def reduce(source_array, starting_point = 0)
+  total = starting_point
+  source_array.reduce {|memo, n| total += (memo + n)}
+  total
+end
