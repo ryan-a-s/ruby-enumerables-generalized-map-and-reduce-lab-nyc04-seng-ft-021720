3e3b69f83fb068cddfd12113733e49ccb3c101a6
Ryan Seit <ryan@ryanseit.com>
2020-02-13 18:38:55 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,12 +1,28 @@
 # Your Code Here
-def map(enumerable, &block)
-  result = []
-  enumerable.map {|element| result << block.call(element)}
-  result
+def map_to_negativize(source_array)
+  source_array.map { |x| -x }
 end
 
-def reduce(source_array, starting_point = 0)
-  total = starting_point
-  source_array.reduce {|memo, n| total += (memo + n)}
-  total
+def map_to_no_change(source_array)
+  source_array.map { |x| x }
+end
+
+def map_to_double(source_array)
+  source_array.map { |x| x*2 }
+end
+
+def map_to_square(source_array)
+  source_array.map { |x| x**2 }
+end
+
+def reduce_to_total(source_array, starting_point = 0)
+  return source_array.reduce(starting_point){|total, element| total+element}
+end
+
+def reduce_to_all_true(source_array)
+   return source_array.reduce{ |x, point| !!x && !!point } 
+end
+
+def reduce_to_any_true(source_array)
+   return source_array.reduce{ |x, point| !!x || !!point } 
 end
