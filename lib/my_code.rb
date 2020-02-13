6919ad969016827a58d7182379b78fae6ded0184
Ryan Seit <ryan@ryanseit.com>
2020-02-13 18:41:00 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,28 +1,25 @@
-# Your Code Here
 def map(source_array)
-  source_array.map { |x| -x }
+  new = []
+  i = 0
+  while i < source_array.length do
+    new.push(yield(source_array[i]))
+    i += 1
+  end
+  return new
 end
 
-def map(source_array)
-  source_array.map { |x| x }
-end
+def reduce(source_array, starting_point = nil)
+  if starting_point
+    sum = starting_point
+    i = 0
+  else
+    sum = source_array[0]
+    i = 1
+  end
 
-def map(source_array)
-  source_array.map { |x| x*2 }
-end
-
-def map(source_array)
-  source_array.map { |x| x**2 }
-end
-
-def reduce_to_total(source_array, starting_point = 0)
-  return source_array.reduce(starting_point){|total, element| total+element}
-end
-
-def reduce_to_all_true(source_array)
-   return source_array.reduce{ |x, point| !!x && !!point } 
-end
-
-def reduce_to_any_true(source_array)
-   return source_array.reduce{ |x, point| !!x || !!point } 
-end
+  while i < source_array.length
+    sum = yield(sum, source_array[i])
+    i += 1
+  end
+  sum
+end 
